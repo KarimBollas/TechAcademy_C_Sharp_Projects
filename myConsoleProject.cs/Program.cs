707082fa837aee2a0b9dfe1324dda4378f34ab34
KarimBollas <emailkarimb@gmail.com>
2021-11-29 15:58:47 -0800

    WRITING AND RUNNING CODE IN C# ASSIGNMENT

---
+++ myConsoleProject.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems");
+            Console.WriteLine("Remember, we\'re \" accounting\" on you!");
+            Console.Read();
+            /*
+             Console.WriteLine("What is your name?");
+             string name=Console.ReadLine();
+             Console.WriteLine("Hello, " + "\""+ name +"\""+"!");
+             Console.Read();
+            */
+        }
+    }
+}
