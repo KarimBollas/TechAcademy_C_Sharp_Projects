9df9b5bea095e5936d6a85b7b5e7f17b12608578
KarimBollas <emailkarimb@gmail.com>
2021-11-30 11:35:39 -0800

    DAILY REPORT SUBMISSION ASSIGNMENT

---
+++ VariablesAndDataTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * string myName = "Karim";
+            byte myNumber = 8;
+            */
+            //This is a simpl
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.Read();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNUmber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNUmber);
+            //int total = favNum + 10;
+            //Console.WriteLine("Your favorite number is: "+favoriteNUmber);
+            //Console.WriteLine("Your favorite number plus 10 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+            
+            double heigthInCentimeters = 171.3021;
+            
+            float secondsLeft = 2.62f;
+            
+            short temperatureInMars = -314;
+            
+            string myName = "Karim";
+            
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+
+            Console.WriteLine("Raining?: "+ rainingStatus);
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ daylyReport/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace daylyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Banner
+            string banner =""+
+                "                  T H E    T E C H    A C A D E M Y\r\n"+
+                "    ^^      ..                                       .. \r\n" +
+                "            []                                       [] \r\n" +
+                "          .:[]:_          ^^                       ,:[]:.\r\n" +
+                "        .: :[]: :-.                             ,-: :[]: :.\r\n" +
+                "      .: : :[]: : :`._                       ,.': : :[]: : :.\r\n" +
+                "    .: : : :[]: : : : :-._               _,-: : : : :[]: : : :.\r\n" +
+                "_..: : : : :[]: : : : : : :-._________.-: : : : : : :[]: : : : :-._\r\n" +
+                "_:_:_:_:_:_:[]:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:[]:_:_:_:_:_:_\r\n" +
+                "!!!!!!!!!!!![]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!![]!!!!!!!!!!!!!\r\n" +
+                "^^^^^^^^^^^^[]^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^[]^^^^^^^^^^^^^\r\n" +
+                "            []                                       []\r\n" +
+                "            []                                       []\r\n" +
+                "            []                                       []\r\n" +
+                " ~~^-~^_~^~/  \\~^-~^~_~^-~_^~-^~_^~~-^~_~^~-~_~-^~_^/  \\~^-~_~^-~~-\r\n" +
+                "~ _~~- ~^-^~-^~~- ^~_^-^~~_ -~^_ -~_-~~^- _~~_~-^_ ~^-^~~-_^-~ ~^\r\n" +
+                "   ~ ^- _~~_-  ~~ _ ~  ^~  - ~~^ _ -  ^~-  ~ _  ~~^  - ~_   - ~^_~\r\n" +
+                "     ~-  ^_  ~^ -  ^~ _ - ~^~ _   _~^~-  _ ~~^ - _ ~ - _ ~~^ -\r\n" +
+                "        ~^ -_ ~^^ -_ ~ _ - _ ~^~-  _~ -_   ~- _ ~^ _ -  ~ ^-\r\n\r\n\r\n\r\n";
+            Console.WriteLine(banner);                      // print Banner
+            Console.WriteLine("What is your name?");        // ask Name
+            string name = Console.ReadLine();                  //read Name
+            Console.WriteLine("What course are you on?");      // ask Course      
+            string course = Console.ReadLine();                 //read Course
+            Console.WriteLine("What page number?");             //ask page
+            int page = Convert.ToInt32(Console.ReadLine());     // cast page to int from input read 
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false.”"); // ask help true or false
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());                                  // cast help to bool from input read, it crashes if not true or false
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics."); // ask experiences
+            string experiences = Console.ReadLine();           // read experiences                      
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");  //ask for feedback
+            string feedback = Console.ReadLine();               //read feedback
+            Console.WriteLine("How many hours did you study today?"); //ask hours of study
+            int hoursStudy = Convert.ToInt32(Console.ReadLine());       //cast hours of study to int from input read
+
+            Console.WriteLine("\n\n Thank you for your answers. \n\rAn Instructor will respond to this shortly. \r\nHave a great day!");
+            Console.ReadLine();
+        }
+    }
+}
