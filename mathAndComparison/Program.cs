e7042fda60b71e591c7c285769f9d75d8a2feb27
KarimBollas <emailkarimb@gmail.com>
2021-11-30 15:22:51 -0800

    MATH AND COMPARISON OPERATOR SUBMISSION ASSIGNMENT add comments to code

--- mathAndComparison/Program.cs
+++ mathAndComparison/Program.cs
@@ -8,23 +8,27 @@ namespace mathAndComparison
         {
             Console.WriteLine("Anonymous Income Comparison Program");
             
+            // Ask for hourly rate and worked hours / week for Person 1
             Console.WriteLine("Person1");
             Console.WriteLine("Hourly Rate?");
             double hourlyRate1 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Hours worked per week?");
             byte hoursWeek1 = Convert.ToByte(Console.ReadLine());
-            
+
+            // Ask for hourly rate and worked hours / week for Person 2
             Console.WriteLine("Person2");
             Console.WriteLine("Hourly Rate?");
             double hourlyRate2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Hours worked per week?");
             byte hoursWeek2 = Convert.ToByte(Console.ReadLine());
 
-            double annualSalary= hourlyRate1 * hoursWeek1*52;
+            //annualSalary is used for both Persons
+            double annualSalary= hourlyRate1 * hoursWeek1*52; //52 weeks per year
             Console.WriteLine("Anual salary Person1: $"+annualSalary);
             annualSalary = hourlyRate2 * hoursWeek2 * 52;
             Console.WriteLine("Anual salary Person2: $" + annualSalary);
-            
+
+            //Compare Person1 and Person2 annualSAlary
             bool greaterThan = (hourlyRate1* hoursWeek1*52) > annualSalary;
             Console.WriteLine("Does Person 1 make more money than Person 2?: " + greaterThan);
 
