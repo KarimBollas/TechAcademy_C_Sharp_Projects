e60665b4858b23b364df38d328596e8ccfbf9ecf
KarimBollas <emailkarimb@gmail.com>
2021-11-30 18:00:08 -0800

    BRANCHING SUBMISSION ASSIGNMENT

---
+++ Conditional/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string color = "red";
+            bool isMexFlag;
+            if (color=="green")
+            {
+                isMexFlag = true;
+            }
+            else if (color=="white")
+            {
+                isMexFlag = true;
+            }
+            else if (color == "red")
+            {
+                isMexFlag = true;
+            }
+            else
+            {
+                isMexFlag = false;
+            }
+            if (isMexFlag)
+            {
+                Console.WriteLine(color + " is a mexican flag color.");
+            }
+            else
+            {
+                Console.WriteLine(color + " is NOT a mexican flag color.");
+            }
+
+            byte age = 17;
+            if (age != 0)
+            {
+                if (age < 18)
+                {
+                    Console.WriteLine("You are still to young to vote in Mexico.");                    
+                }
+                else
+                {
+                    Console.WriteLine("You are an adult in Mexico.");
+                }
+                if (age >= 65)
+                {
+                    Console.WriteLine("You can ask for government help in Mexico.");
+                }
+
+                
+                int daysMonth = 29;
+                string month = (daysMonth == 28 || daysMonth == 29) ? "February": "Not February";
+                Console.WriteLine(month);
+
+                int currentTemp = 80;
+                int roomTemp = 70;
+
+                if (currentTemp==roomTemp)
+                {
+                    Console.WriteLine("It is exactly room temperature.");
+                }
+                else if (currentTemp > roomTemp)
+                {
+                    Console.WriteLine("It is warmer than room temperature.");
+                }
+                else if (currentTemp < roomTemp)
+                {
+                    Console.WriteLine("Room temperature is warmer than current temperature.");
+                }
+                else
+                {
+                    Console.WriteLine("It is NOT exactly  room temperature.");
+                }
+
+                string comparisonResult = currentTemp == roomTemp ? "It is troom temp." : "It is not room temp.";
+                Console.WriteLine(comparisonResult);
+
+                Console.WriteLine("What is your name?");
+                string name = Console.ReadLine();
+                Console.WriteLine("Hello, "+ name+ ", what is the temperature where you are?");
+                currentTemp = Convert.ToInt32(Console.ReadLine());
+
+                if (currentTemp==roomTemp)
+                {
+                    Console.WriteLine("It is exactly room temperature.");
+                }
+                else if (currentTemp > roomTemp)
+                {
+                    Console.WriteLine("It is warmer than room temperature.");
+                }
+                else if (currentTemp < roomTemp)
+                {
+                    Console.WriteLine("It is colder than room temperature.");
+                }
+                else
+                {
+                    Console.WriteLine("Uhhhhh.... something were wrong!!");
+                }
+
+                Console.WriteLine("What is your favorite number?");
+                int favNum = Convert.ToInt32(Console.ReadLine());
+
+                string result = favNum == 8 ? "You have an awesome fovorite number." : "You do NOT have an awesome fovorite number.";
+
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
---
+++ shippingQuote/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace shippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. \n\rPlease follow the instructions below.\n\n");
+            Console.WriteLine("What is the weight of your package? ");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight>50) //if weight greater than 50 send error and program ends.
+            {
+                Console.WriteLine("Error Message: Package too heavy to be shipped via Package Express. :( \n Have a good day. :) ");
+            }
+            else // if package not heavy ask for width, height and length
+            {
+                Console.WriteLine("What is the width of your package? ");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the height of your package? ");
+                int height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the length of your package? ");
+                int length = Convert.ToInt32(Console.ReadLine());
+
+                int size = width + height + length;
+                if (size>50) //if size greater than 50 send error and program ends.
+                {
+                    Console.WriteLine("Error Message: Package too big to be shipped via Package Express. ");
+                }
+                else //otherwise calculates quote
+                {
+                    double quote = ((double)width * (double)height * (double)length) * (double)weight / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + quote);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
