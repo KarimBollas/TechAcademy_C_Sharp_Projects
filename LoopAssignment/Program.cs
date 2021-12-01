523ff3193008ec90fb90edcc37eeaa8574156fee
KarimBollas <emailkarimb@gmail.com>
2021-12-01 11:13:37 -0800

    CONSOLE APP SUBMISSION ASSIGNMENT

---
+++ LoopAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace LoopAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number (from 2 to 2,147,483,647)"); 
+            int limit = Convert.ToInt32( Console.ReadLine() ); //read a number form input
+            bool isPrime;          //bool to determinate a prime number
+            int n=limit;            //limit to calculate prime numbers            
+            int factor;             // factor to divide number (n)
+            
+
+            if (n>=2 && n<= 2147483647)             // is n in range
+            {
+                Console.WriteLine("Prime Numbers up to " + limit);
+                while (n >= 3)              // 
+                {
+                    isPrime = true;         // consider n as a prime until find a divisor
+                    factor = 2;             // factor as a divisor starts at 2
+                    do
+                    {                        
+                        if ((n % factor)==0)    //factor divides n, if true NOT prime
+                        {
+                            isPrime = false;
+                        }
+                        factor++;           
+                    } while (factor <= Math.Sqrt(n));  // calculate up to square root of n
+                    if (isPrime)    //if isPrime print it
+                    {
+                        Console.WriteLine(n);
+                    }
+                    n--;    //next number 
+                }
+                
+                Console.WriteLine("2"); // first prime number
+
+            }
+            else
+            {
+                Console.WriteLine("Not a valid number (from 2 to 2,147,483,647). Program ends");
+            }
+
+            
+            
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool isGuessed = false;
+
+            //while (!isGuessed)
+            //{
+            //    Console.WriteLine("Guess a number?");
+            //    int number = Convert.ToInt32(Console.ReadLine());
+            //    switch (number)
+            //    {
+            //        case 12:
+            //            Console.WriteLine("You guessed 12. Congrats.");
+            //            isGuessed = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("You guessed " + number +". Try again.");
+            //            break;                    
+            //    }
+            //}
+
+
+            do
+            {
+                Console.WriteLine("Guess a number?");
+                int number = Convert.ToInt32(Console.ReadLine());
+                switch (number)
+                {
+                    case 12:
+                        Console.WriteLine("You guessed 12. Congrats.");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You guessed " + number + ". Try again.");
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+
+            Console.Read();
+        }
+    }
+}
