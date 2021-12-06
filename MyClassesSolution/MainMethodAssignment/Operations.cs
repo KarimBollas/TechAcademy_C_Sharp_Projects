b628874a7bfa029fab427a2c6903b34efd89a962
KarimBollas <emailkarimb@gmail.com>
2021-12-05 23:11:33 -0800

    MAIN METHOD SUBMISSION ASSIGNMENT

---
+++ MyClassesSolution/MainMethodAssignment/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MainMethodAssignment
+{
+    public class Operations
+    {
+
+        //method that will take in an integer
+        public int ConvertFromKBTob(int kilobytes) 
+        {            
+            return (kilobytes*1024*8); // operation for the integer then return the answer as an integer
+        }
+
+        //overload method ConvertFromKBTob that will take in a decimal
+        public int ConvertFromKBTob(decimal kilobytes)
+        {
+            return ((int)(kilobytes)* 1024 * 8)+1; // different operation for the decimal (+1) then return the answer as an integer
+        }
+
+        //overload method ConvertFromKBTob that will take in a string
+        public int ConvertFromKBTob(string kilobytes)
+        {            
+            return (Convert.ToInt32(kilobytes) * 1024 * 8) -1; // (first convert to integer), different operation (-1)  then return the answer as an integer
+        }
+    }
+}
--- MyClassesSolution/MainMethodAssignment/Program.cs
+++ MyClassesSolution/MainMethodAssignment/Program.cs
@@ -6,7 +6,23 @@ namespace MainMethodAssignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // instantiate the class Operations.
+            Operations operations = new Operations();
+            //call the one method, passing in an integer and display the result to the screen.
+            Console.WriteLine("500 KB equals to: "+operations.ConvertFromKBTob(500)+" bits");
+
+            // instantiate the class Operations
+            Operations operations2 = new Operations();
+            //call the second  method, passing in a decimal and display the result to the screen.
+            Console.WriteLine("500 KB equals to: " + operations2.ConvertFromKBTob(500m) + " bits");
+
+            // instantiate the class Operations
+            Operations operations3 = new Operations();
+            //call the third  method, passing in a string and display the result to the screen.
+            Console.WriteLine("500 KB equals to: " + operations3.ConvertFromKBTob("500") + " bits");
+
+            Console.ReadLine();
         }
     }
 }
