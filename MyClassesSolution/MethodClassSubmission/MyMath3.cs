19d41654b91170d8f2d837b3068a3153256da03d
KarimBollas <emailkarimb@gmail.com>
2021-12-06 01:07:10 -0800

    METHOD CLASS SUBMISSION ASSIGNMENT

---
+++ MyClassesSolution/MethodClassSubmission/MyMath3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MethodClassSubmission
+{
+    class MyMath3
+    {
+        public void Factorial(out int factorial, int number)
+        {
+            //initilize factorial as number
+            factorial = number;
+            //runs trhough whole numbers from number to 1 and multiply 
+            for (int i=number; i > 2; i--)
+            {
+                factorial = factorial * (i - 1);
+            }
+            Console.Write(number);
+        }
+    }
+}
---
+++ MyClassesSolution/MethodClassSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //instantiate  the class MyMath3
+            MyMath3 myOPeration = new MyMath3();
+
+            int factorial;
+
+            Console.Write("Factorial of ");
+            //calls Factorial Method, returns factorial of second parameter in 'factorial', and print second parameter
+            myOPeration.Factorial( factorial: out factorial, number: 5);
+            //prints factorial
+            Console.Write(" is {0}", factorial);
+            Console.ReadLine();
+        }
+    }
+}
