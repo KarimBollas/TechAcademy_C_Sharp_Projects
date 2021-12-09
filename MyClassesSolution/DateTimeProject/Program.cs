971e56fc906ad05e066ca900cdcd8f6ab2dcabd0
KarimBollas <emailkarimb@gmail.com>
2021-12-08 18:39:37 -0800

    DATE/TIME SUBMISSION ASSIGNMENT

---
+++ MyClassesSolution/DateTimeProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DateTimeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime dateTime = new DateTime(1987, 2, 9, 18, 10, 5);
+            //Console.WriteLine(dateTime.ToString());
+
+            //Print to console current date
+            DateTime justNow= DateTime.Now;
+            Console.WriteLine(justNow);
+
+            //Asks for a number.
+            Console.WriteLine("How many hours do you want to add ?");
+            double hours = Convert.ToDouble(Console.ReadLine());
+
+            //Add hours to datatime variable
+            
+            Console.WriteLine(justNow.AddHours(hours));
+
+            Console.ReadLine();
+        }
+    }
+        
+}
